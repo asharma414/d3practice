520085874f65f6718a3ba7a0039910f37b7c1c03
asharma414 <abhi.sharm.414@gmail.com>
2020-10-03 23:12:51 -0400

    set up chart axes

--- index.js
+++ index.js
@@ -2,6 +2,69 @@ const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
+const render = data => {
+    const title = 'Bitcoin Price History';
+  
+    const xValue = d => d.date;
+    const xAxisLabel = 'Date';
+    
+    const yValue = d => d.close;
+    const circleRadius = 2;
+    const yAxisLabel = 'Price';
+    
+    const margin = { top: 60, right: 40, bottom: 88, left: 250 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(data, xValue))
+      .range([0, innerWidth])
+    
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, yValue))
+      .range([innerHeight, 0])
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+    
+    const xAxis = d3.axisBottom(xScale)
+      .tickSize(-innerHeight)
+      .tickPadding(15);
+    
+    const yAxis = d3.axisLeft(yScale)
+      .tickSize(-innerWidth)
+      .tickPadding(10);
+    
+    const yAxisG = g.append('g').call(yAxis);
+    yAxisG.selectAll('.domain').remove();
+    
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', -100)
+        .attr('x', -innerHeight / 2)
+        .attr('fill', 'black')
+        .attr('transform', `rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .text(yAxisLabel);
+    
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`);
+    
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 80)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabel);
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text(title);
+}
+
 d3.csv('https://gist.github.com/asharma414/0a5acc74925a0feb6ebd352fed4fbdf6/raw/78da52a64dccc63c745c4a6f7d89fdd0e35d8444/btc.csv')
     .then(data => {
         data.forEach(d => {
